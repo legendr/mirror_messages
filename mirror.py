96dc6b1e2358733e0ec4f24aa7a04d9e533bcd3f
legendr <legendr@mail.ru>
2022-03-25 00:36:09 +0300

    replace hash on list, print log

--- mirror.py
+++ mirror.py
@@ -1,8 +1,8 @@
 
+from datetime import datetime
 from telethon import events
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
-from pymongo import MongoClient
 import redis
 from environs import Env
 
@@ -12,36 +12,35 @@ env.read_env()
 API_ID = env.int('API_ID')
 API_HASH = env.str('API_HASH')
 SESSION_STRING = env.str('SESSION_STRING')
-DATABASE_URL = env.str('DATABASE_URL')
 SOURCE_CHANNELS = env.list('SOURCE_CHANNELS')
 MY_CHANNEL=env.int('MY_CHANNEL')
 
 r = redis.Redis(host='redis', port=6379)
-client_mndb = MongoClient(DATABASE_URL)
-db = client_mndb.db_telegram
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
-# Обработчик новых сообщений
 @client.on(events.NewMessage(chats=SOURCE_CHANNELS))
 async def handler_new_message(event):
     try:
         if event.message.to_dict()['fwd_from'] == None:
             channel_id=event.message.to_dict()['peer_id']['channel_id']
             post_id=event.message.to_dict()['id']
+            print(f"{datetime.now()} ORIGINAL {channel_id}:{post_id}")
         else:
             channel_id=event.message.to_dict()['fwd_from']['from_id']['channel_id']
             post_id=event.message.to_dict()['fwd_from']['channel_post']
+            print(f"{datetime.now()} FORWARD {channel_id}:{post_id}")
         if r.exists('channel') == 0:
-            r.hsetnx('channel', channel_id, post_id)
-            r.expire('channel',600)
+            r.sadd('channel', f'{channel_id}:{post_id}')
+            r.expire('channel', 86400)
             await client.forward_messages(MY_CHANNEL, event.message)
+            print(f"{datetime.now()} NOT EXISTS->ADD IN SET->SEND MESSAGE {channel_id}:{post_id}")
         else:
-            if r.hsetnx('channel', channel_id, post_id) == 1:
+            if r.sismember('channel', f'{channel_id}:{post_id}') == 0:
+                r.sadd('channel', f'{channel_id}:{post_id}')
                 await client.forward_messages(MY_CHANNEL, event.message)
+                print(f"{datetime.now()} ADD TO SET->SEND MESSAGE {channel_id}:{post_id}")
             else:
-                print("COPY")
-        # отправим сообщение в наш канал
-        #await client.send_message(TARGET_CHANNEL, event.message)
+                print(f"{datetime.now()} COPY {channel_id}:{post_id}")
  
     except Exception as e:
         print(e)
